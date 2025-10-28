d9fb774222d0f2de328cf75d23bbe210536d1074
Chirag <chirag_wadhwani289@hotmail.com>
2025-10-28 22:09:28 +0700

    updated db username

--- sportsleaguerankingtable/settings.py
+++ sportsleaguerankingtable/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sportsleague',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
         'HOST': 'localhost',
         'PORT': '3306',
     }
